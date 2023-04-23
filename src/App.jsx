2cde3b7cfab204a2f9bd39f1bf4d9599a29bdd18
MichaeTav <m.tavera@hotmail.com>
2023-04-22 20:25:07 -0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Gov Sort</h1>
+        <h2>Choose a Topic!</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
