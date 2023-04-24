8c304c944ff659c79956252ad6c9c90034559c1b
MichaeTav <m.tavera@hotmail.com>
2023-04-24 13:25:11 -0700

    extracted code to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
 import "./App.css";
 import TopicCard from "./components/TopicCard";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
-      <div className="container">
-        <h1>Gov Sort</h1>
-        <div className="topics">
-          <h2>Choose a Topic!</h2>
-          <div className="topic-card-container">
-            <TopicCard
-              topic="Test"
-              description="test description test description test description test description test description"
-            />
-            <TopicCard
-              topic="Test"
-              description="test description test description test description test description test description"
-            />
-            <TopicCard
-              topic="Test"
-              description="test description test description test description test description test description"
-            />
-          </div>
-        </div>
-      </div>
+      <Home />
     </>
   );
 }
--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const TopicCard = ({ topic, description }) => {
+const TopicCard = ({ title, description }) => {
   return (
     <div className="topic-card">
-      <h2>{topic}</h2>
+      <h2>{title}</h2>
       <p>{description}</p>
       <button>PLAY</button>
     </div>
