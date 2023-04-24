62c69a361c4c4be9906135b5c3d3c8c8f2f189a1
MichaeTav <m.tavera@hotmail.com>
2023-04-24 13:24:11 -0700

    added topic data

---
+++ src/data/topics.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: "Amendments",
+    description: "Sort amendments by number",
+  },
+  {
+    title: "Presidents",
+    description: "Sort Presidents by inauguration date",
+  },
+  {
+    title: "Supreme Court Cases",
+    description: "Sort famous Supreme Court Cases by decision date",
+  },
+];
