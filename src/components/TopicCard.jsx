15251b2475c6357423936eb39d16f90fbe15baf9
MichaeTav <m.tavera@hotmail.com>
2023-04-25 12:36:07 -0700

    test

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
-const TopicCard = ({ title, description }) => {
+const TopicCard = ({ title, description, callback }) => {
+  const selectTopic = (event) => {
+    event.preventDefault();
+    callback(title);
+  };
   return (
     <div className="topic-card">
       <h2>{title}</h2>
       <p>{description}</p>
-      <button>PLAY</button>
+      <button onClick={(e) => selectTopic(e)}>PLAY</button>
     </div>
   );
 };
