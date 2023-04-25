35433e578495e6ab21d0542962e6810e37d8d707
MichaeTav <m.tavera@hotmail.com>
2023-04-25 12:36:35 -0700

    history

---
+++ src/data/unitedStatesHistory.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    order: 1776,
+    description: "Declaration of Independence",
+    url: "",
+  },
+  {
+    order: 1787,
+    description: "U.S. Constitution drafted",
+    url: "",
+  },
+  {
+    order: 1789,
+    description: "George Washington becomes the first U.S. President",
+    url: "",
+  },
+  {
+    order: 1803,
+    description: "Louisiana Purchase",
+    url: "",
+  },
+  {
+    order: 1861,
+    description: "American Civil War begins",
+    url: "",
+  },
+  {
+    order: 1865,
+    description: "American Civil War ends",
+    url: "",
+  },
+  {
+    order: 1920,
+    description: "19th Amendment grants women the right to vote",
+    url: "",
+  },
+  {
+    order: 1929,
+    description: "Stock Market Crash and the beginning of the Great Depression",
+    url: "",
+  },
+  {
+    order: 1941,
+    description: "Pearl Harbor attack and U.S. entry into World War II",
+    url: "",
+  },
+  {
+    order: 1945,
+    description: "End of World War II",
+    url: "",
+  },
+  {
+    order: 1950,
+    description: "Korean War begins",
+    url: "",
+  },
+  {
+    order: 1953,
+    description: "Korean War ends",
+    url: "",
+  },
+  {
+    order: 1954,
+    description: "Brown v. Board of Education ruling",
+    url: "",
+  },
+  {
+    order: 1964,
+    description: "Civil Rights Act signed into law",
+    url: "",
+  },
+  {
+    order: 1969,
+    description: "Apollo 11 moon landing",
+    url: "",
+  },
+  {
+    order: 2001,
+    description: "September 11 terrorist attacks",
+    url: "",
+  },
+  {
+    order: 2008,
+    description: "Global financial crisis",
+    url: "",
+  },
+  {
+    order: 2020,
+    description: "COVID-19 pandemic",
+    url: "",
+  },
+];
