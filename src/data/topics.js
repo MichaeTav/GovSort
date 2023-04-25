ec76efde5327a5308b690a96ca86b2a87d1e11ee
MichaeTav <m.tavera@hotmail.com>
2023-04-25 12:36:40 -0700

    added new topics

--- src/data/topics.js
+++ src/data/topics.js
@@ -8,7 +8,16 @@ export default [
     description: "Sort Presidents by inauguration date",
   },
   {
-    title: "Supreme Court Cases",
+    title: "Supreme Court",
     description: "Sort famous Supreme Court Cases by decision date",
+  },
+  {
+    title: "U.S. History",
+    description:
+      "Sort key events in United States History from declaring independence to the covid pandemic",
+  },
+  {
+    title: "TEST",
+    description: "TESTING",
   },
 ];
